f412fb02e7479b66a3114c76392aa4238f15ac2a
JoshBergman <111944876+JoshBergman@users.noreply.github.com>
2022-10-31 20:57:18 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import PageTemplate from './Components/UI/PageResources/PageTemplate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageTemplate>
+      This is words
+    </PageTemplate>
   );
 }
 
